fc4cd325826c5d3983d50a8690b7ae3a7ca07411
notevgeny <gcause17@gmail.com>
2020-01-23 22:16:03 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let money = '2000$',
+    income = true, 
+    addExpenses = false, 
+    deposit = 5000, 
+    mission = 'click any button to start', 
+    period = 12;
+
+alert(money);
+console.log(mission);
