61c865847e9635a365ba69910ec31c33642ece2a
notevgeny <gcause17@gmail.com>
2020-01-24 21:42:54 +0300

    added lesson2

--- main.js
+++
@@ -1,9 +0,0 @@
-let money = '2000$',
-    income = true, 
-    addExpenses = false, 
-    deposit = 5000, 
-    mission = 'click any button to start', 
-    period = 12;
-
-alert(money);
-console.log(mission);
